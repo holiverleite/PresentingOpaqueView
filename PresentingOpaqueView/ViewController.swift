17ad9f0c800feb89a74388f542d68c4e1e42a9c8
Haroldo Leite <holiverleite@gmail.com>
2020-04-03 15:21:52 -0300

    Initial Commit

---
+++ PresentingOpaqueView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PresentingOpaqueView
+//
+//  Created by Haroldo on 03/04/20.
+//  Copyright © 2020 Haroldo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
