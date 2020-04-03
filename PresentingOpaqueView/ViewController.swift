77b238c218d60977a049e010d48b7b8dbcc0b412
Haroldo Leite <holiverleite@gmail.com>
2020-04-03 15:49:05 -0300

    --

--- PresentingOpaqueView/ViewController.swift
+++ PresentingOpaqueView/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tapHereButton: UIButton! {
+        didSet {
+            self.tapHereButton.addTarget(self, action: #selector(buttonDidTap), for: .touchUpInside)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @objc func buttonDidTap() {
+        //abreview
+        let mainView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+        mainView.backgroundColor = UIColor.gray.withAlphaComponent(0.3)
+        
+        let cardView = UIView(frame: CGRect(x: 0, y: self.view.frame.height, width: self.view.frame.width, height: self.view.frame.height * 0.2))
+        cardView.backgroundColor = .white
+        
+        self.view.addSubview(mainView)
+        self.view.addSubview(cardView)
+        
+        UIView.animate(withDuration: 1.3, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: { (finished) in
+            if finished {
+                UIView.animate(withDuration: 0.3, delay: 0.3, options: .curveEaseOut, animations: {
+                    cardView.frame = CGRect(x: 0, y: self.view.frame.height - cardView.frame.height, width: self.view.frame.width, height: self.view.frame.height * 0.2)
+                }, completion: nil)
+            }
+        })
 
+    }
 }
 
